a9bf560f57bab5c43061612f2b18e6f0fb3dda30
sharonandisi <sharonandisi.28@gmail.com>
2019-07-18 19:41:22 +0300

    [#feature 167193120] create the post property test

---
+++ server/usingDB/tests/property.test.js
@@ -0,0 +1,33 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../server';
+import { createAllTables } from '../../../db';
+
+const { expect } = chai;
+chai.should();
+chai.use(chaiHttp);
+
+
+
+describe('/POST property', () => {
+    it('should successfully post a property advert', (done) => {
+        chai.request(app)
+            .post('/api/v1/property')
+
+            .attach('image_url', testImage)
+            .field({
+                status: 'Available',
+                price: 5000000,
+                state: 'Nairobi',
+                city: 'Nairobi',
+                address: 'Kenya',
+                type: '2 bedroom'
+            })
+            .end((err, res) => {
+                res.should.have.status(201);
+                if (err) return done();
+                done();
+            });
+    });
+
+})
